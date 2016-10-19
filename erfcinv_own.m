08bb6899a2a0c3b597704180f9213f61fa861804
KaySteinkamp <kay.steinkamp@gmail.com>
2016-10-19 11:19:23 +0100

    Renamed erfcinv to erfcinv_own to avoid shadowing

--- erfcinv_own.m
+++ erfcinv_own.m
@@ -1,15 +1,9 @@
-function p = erfcinv(z)
+function p = erfcinv_own(z)
 %ERFCINV Inverse incomplete error function.
 %
 %   X = ERFCINV(Y) is the inverse incomplete error function for each element of
 %   Y.  The incomplete inverse error functions satisfies y = erfc(x), for 0 <=
 %   y <= 2 and -inf <= x <= inf.
 %
-%   See also ERFC, ERF.
-
-%   Author:      Peter J. Acklam
-%   Time-stamp:  2003-04-13 11:45:24 +0200
-%   E-mail:      pjacklam@online.no
-%   URL:         http://home.online.no/~pjacklam
 
 p = -ltqnorm(z / 2) / sqrt(2);
